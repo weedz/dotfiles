3cacd38c8a3139693fca9045440a891e4fec3433
Linus Björklund <weedzcokie@gmail.com>
2024-02-20 00:51:53 +0100

    nvim: Replace `nvim-tree` with `mini.files`

---
+++ .config/nvim/lua/plugins/mini-files.lua
@@ -0,0 +1,8 @@
+return {
+    "echasnovski/mini.files",
+    opts = {
+        options = {
+            use_as_default_explorer = true,
+        },
+    },
+}
--- .config/nvim/lua/plugins/nvim-tree.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "nvim-tree/nvim-tree.lua",
-    lazy = false,
-    opts = {
-        git = {
-            ignore = false,
-        },
-    },
-}
