a6b8ae67ea12745835f50df79aff996e094bb480
Linus Björklund <weedzcokie@gmail.com>
2024-02-20 16:51:15 +0100

    nvim: Lazy `attempt` init

--- .config/nvim/lua/plugins/attempt.lua
+++ .config/nvim/lua/plugins/attempt.lua
@@ -1,18 +1,24 @@
 return {
     "m-demare/attempt.nvim",
+    lazy = true,
     opts = {
         run = {
             ts = { "w", "!tsx %:p" },
         },
         ext_options = { "ts", "js", "lua", "sh", "py" },
     },
-    init = function()
-        local wk = require("which-key")
-        wk.register({
-            ["<leader>a"] = { name = "+attempt" },
-        })
-        require("telescope").load_extension("attempt")
-    end,
+    keys = {
+        { "<leader>a", desc = "+attempt" },
+        { "<leader>an", '<cmd>lua require("attempt").new_select()<cr>', desc = "New attempt (with extenstion)" },
+        { "<leader>ai", '<cmd>lua require("attempt").new_input_ext()<cr>', desc = "New" },
+        { "<leader>ar", '<cmd>lua require("attempt").run()<cr>', desc = "Run" },
+        { "<leader>ad", '<cmd>lua require("attempt").delete_buf()<cr>', desc = "Delete buffer" },
+        { "<leader>ac", '<cmd>lua require("attempt").rename_buf()<cr>', desc = "Rename" },
+        { "<leader>al", "<cmd>Telescope attempt<cr>", desc = "Telescope" },
+    },
+    -- init = function()
+    --     -- require("telescope").load_extension("attempt")
+    -- end,
     dependencies = {
         "nvim-telescope/telescope.nvim",
         "nvim-lua/plenary.nvim",
