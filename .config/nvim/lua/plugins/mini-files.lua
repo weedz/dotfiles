8b43026d1d989067d52a30a25b5f10044e9cdcce
Linus Björklund <weedzcokie@gmail.com>
2024-02-20 16:11:43 +0100

    nvim: Add `fugitive` and `undotree`

--- .config/nvim/lua/plugins/mini-files.lua
+++ .config/nvim/lua/plugins/mini-files.lua
@@ -1,5 +1,6 @@
 return {
     "echasnovski/mini.files",
+    lazy = false,
     opts = {
         options = {
             use_as_default_explorer = true,
---
+++ .config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,8 @@
+return {
+    "mbbill/undotree",
+    -- opts = {},
+    config = function() end,
+    keys = {
+        { "<leader><F5>", "<cmd>UndotreeToggle<CR>", desc = "Undotree" },
+    },
+}
