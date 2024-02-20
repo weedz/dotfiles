c6fd166768f8ab8612d0ffaade4c0a29f82937ff
Linus Björklund <weedzcokie@gmail.com>
2024-02-20 23:53:17 +0100

    nvim: Some trouble keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Telescope
 vim.keymap.set("n", "<leader>pw", function()
     local word = vim.fn.expand("<cword>")
     require("telescope.builtin").grep_string({ search = word })
@@ -12,3 +14,14 @@ end, { desc = "Find WORD at cursor" })
 vim.keymap.set("n", "<leader>pf", function()
     require("telescope.builtin").find_files()
 end, { desc = "Find files" })
+
+-- Trouble
+vim.keymap.set("n", "<leader>tt", function()
+    require("trouble").toggle()
+end)
+vim.keymap.set("n", "<leader>tn", function()
+    require("trouble").next({ skip_groups = true, jump = true })
+end)
+vim.keymap.set("n", "<leader>tp", function()
+    require("trouble").previous({ skip_groups = true, jump = true })
+end)
