3bac27aff924f81b039bb4932f316e67acf7a6b5
Linus Björklund <weedzcokie@gmail.com>
2024-07-25 01:06:46 +0200

    nvim: Disable markdown plugin
    Conceals backtick and other important characters...

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
     { "lukas-reineke/headlines.nvim", enabled = false },
     { "echasnovski/mini.pairs", enabled = false },
     { "folke/flash.nvim", enabled = false },
+    { "MeanderingProgrammer/markdown.nvim", enabled = false },
 }
