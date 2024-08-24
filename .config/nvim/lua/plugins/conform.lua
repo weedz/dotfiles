fcb49ef1084769cd3deae903cce2b74c6ae55dea
Linus Björklund <weedzcokie@gmail.com>
2024-08-24 22:32:18 +0200

    nvim: Add dprint

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,18 +2,24 @@ return {
     "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = {
-            typescript = { "prettierd" },
-            typescriptreact = { "prettierd" },
+            typescript = { "prettierd", "dprint" },
+            typescriptreact = { "prettierd", "dprint" },
+            json = { "jsonls" },
         },
         formatters = {
             prettierd = {
                 -- Make sure to only run prettier if the directory contains a '.prettierrc' file
-                condition = function(self, ctx)
+                condition = function(ctx)
                     return vim.fs.find(".prettierrc", {
                         path = ctx.filename,
                         upward = true,
                         stop = vim.fs.dirname(vim.fn.getcwd()),
                     })[1]
+                end,
+            },
+            dprint = {
+                condition = function(ctx)
+                    return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
                 end,
             },
         },
