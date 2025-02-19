4a341a140f9ed00df9f0d13a753d880f33bdefca
Linus Björklund <weedzcokie@gmail.com>
2025-02-19 08:55:10 +0100

    nvim: Fix blink defaults

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -3,6 +3,14 @@ return {
         "saghen/blink.cmp",
         opts = {
             completion = {
+                list = {
+                    selection = {
+                        auto_insert = false,
+                    },
+                },
+                menu = {
+                    auto_show = false,
+                },
                 accept = {
                     auto_brackets = {
                         enabled = false,
