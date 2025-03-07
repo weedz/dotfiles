776995981d1ccddff5e553bd65b315e7f0fdbd8b
Linus Björklund <weedzcokie@gmail.com>
2025-03-07 11:46:30 +0100

    nvim: Use `snacks.picker` and add `Variable` to "kind filter"
    `Variable` will show global constants/variabes in js/ts. Eg. `const foo
    = () => {}`.

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -3,6 +3,26 @@ return {
         "LazyVim/LazyVim",
         opts = {
             colorscheme = "rose-pine-main",
+            -- From <https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/init.lua#L101>
+            kind_filter = {
+                default = {
+                    "Class",
+                    "Constructor",
+                    "Constant", -- added
+                    "Enum",
+                    "Field",
+                    "Function",
+                    "Interface",
+                    "Method",
+                    "Module",
+                    "Namespace",
+                    "Package",
+                    "Property",
+                    "Struct",
+                    "Trait",
+                    "Variable", -- added. For global variables (eg. `const foo = () => {}`)
+                },
+            },
         },
     },
 }
