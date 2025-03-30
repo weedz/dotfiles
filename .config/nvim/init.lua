d7b79d431b57ab1d0f313e09c13bfae90b779cc6
Linus Björklund <weedzcokie@gmail.com>
2025-03-30 13:17:51 +0200

    nvim: Set diagnostic.virtual_lines

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,3 +15,14 @@ require("config.lazy")
 
 -- require private/excluded stuff, if exists
 pcall(require, "private.autocmds")
+
+vim.diagnostic.config({
+    -- Use the default configuration
+    -- virtual_lines = true,
+
+    -- Alternatively, customize specific options
+    virtual_lines = {
+        -- Only show virtual line diagnostics for the current cursor line
+        current_line = true,
+    },
+})
