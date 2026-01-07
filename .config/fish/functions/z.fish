e3108eda66429cbab2842614ef8c747158e696d7
Linus Björklund <weedzcokie@gmail.com>
2026-01-07 23:45:35 +0100

    tmux and tmux-sessionizer

--- .config/fish/functions/z.fish
+++ .config/fish/functions/z.fish
@@ -1,6 +1,6 @@
 function z
     # Use `fzf` to `cd` into a directory
-    set DIR $(fd --type=directory --absolute-path --exclude='node_modules' --exclude='node_modules.asar.unpacked' --exclude='dist' --exclude='vendor' --exclude='target' . ~/Documents/ | fzf-tmux)
+    set DIR $(fd --type=directory --absolute-path --exclude='node_modules' --exclude='node_modules.asar.unpacked' --exclude='dist' --exclude='vendor' --exclude='target' . ~/Documents/ | fzf-tmux --scheme=path)
 
     if test -d "$DIR"
         cd "$DIR" || exit 1
