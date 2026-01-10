4e9f5b686317778e4e7559c5fae692d9ec7ca0d1
Linus Björklund <weedzcokie@gmail.com>
2026-01-10 01:08:27 +0100

    nvim: Use oxfmt
    Something with `oxfmt --stdin-path` exits after exactly 146177 bytes, if
    the file is larger it will be cut. Define oxfmt with `stdin = false`,
    which uses some "tmp file" mechanism instead.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,36 +1,19 @@
+local util = require("conform.util")
+
 return {
     "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = {
-            typescript = { "prettierd", "dprint" },
-            typescriptreact = { "prettierd", "dprint" },
-            json = { "jsonls" },
+            typescript = { "oxfmt" },
+            typescriptreact = { "oxfmt" },
+            json = { "oxfmt" },
         },
         formatters = {
-            prettier = {
-                -- Make sure to only run prettier if the directory contains a '.prettierrc' file
-                condition = function(ctx)
-                    return vim.fs.find(".prettierrc", {
-                        path = ctx.filename,
-                        upward = true,
-                        stop = vim.fs.dirname(vim.fn.getcwd()),
-                    })[1]
-                end,
-            },
-            prettierd = {
-                -- Make sure to only run prettier if the directory contains a '.prettierrc' file
-                condition = function(ctx)
-                    return vim.fs.find(".prettierrc", {
-                        path = ctx.filename,
-                        upward = true,
-                        stop = vim.fs.dirname(vim.fn.getcwd()),
-                    })[1]
-                end,
-            },
-            dprint = {
-                condition = function(ctx)
-                    return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-                end,
+            oxfmt = {
+                command = util.from_node_modules("oxfmt"),
+                -- can't use `--stdin-filepath`. something dies after exactly 146177 bytes
+                args = { "$FILENAME" },
+                stdin = false,
             },
         },
     },
